f0f8521f7f3ddbfcac460c4ffd9318d91637f9ea
Pedro Hernandez <pihebor@hotmail.com>
2020-05-29 19:43:43 -0500

    interfaz crear persona terminada, clase persona y datos terminada

--- app/src/main/java/com/example/personas_material/Datos.java
+++ app/src/main/java/com/example/personas_material/Datos.java
@@ -1,4 +1,15 @@
 package com.example.personas_material;
 
+import java.util.ArrayList;
+
 public class Datos {
+    private static ArrayList<Persona> personas = new ArrayList<>();
+    public static void guardar(Persona p){
+        personas.add(p);
+    }
+
+    public static ArrayList<Persona> obtener(){
+        return personas;
+    }
+
 }
---
+++ app/src/main/java/com/example/personas_material/DetallePErsona.java
@@ -0,0 +1,14 @@
+package com.example.personas_material;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetallePErsona extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalle_p_ersona);
+    }
+}
