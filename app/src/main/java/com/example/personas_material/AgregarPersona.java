912f9d667d6ed43bfaf57cf157b50e060e7f023c
Pedro Hernandez <pihebor@hotmail.com>
2020-05-29 21:21:07 -0500

    Interfaz itme PErsonas terminada, queda pendiente que la imagen sea circular

--- app/src/main/java/com/example/personas_material/AgregarPersona.java
+++ app/src/main/java/com/example/personas_material/AgregarPersona.java
@@ -2,13 +2,73 @@ package com.example.personas_material;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.ArrayList;
+import java.util.Random;
 
 public class AgregarPersona extends AppCompatActivity {
-
+    private ArrayList<Integer> fotos;
+    private EditText cedula, nombre, apellido;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_agregar_persona);
+
+        cedula = findViewById(R.id.txtCedula);
+        nombre = findViewById(R.id.txtNombre);
+        apellido = findViewById(R.id.txtApellido);
+
+        fotos = new ArrayList<>();
+        fotos.add(R.drawable.images);
+        fotos.add(R.drawable.images2);
+        fotos.add(R.drawable.images3);
+
+    }
+
+    public void guardar(View v){
+        String ced, nom, apell;
+        int foto;
+        Persona persona;
+        InputMethodManager imp= (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        ced = cedula.getText().toString();
+        nom = nombre.getText().toString();
+        apell = apellido.getText().toString();
+        foto = foto_aleatoria();
+        persona =  new Persona(ced, nom, apell, foto);
+        persona.guardar();
+        limpiar();
+        imp.hideSoftInputFromWindow(cedula.getWindowToken(), 0);
+        Snackbar.make(v, getString(R.string.mensaje_guardado_correcto),Snackbar.LENGTH_LONG).show();
+    }
+
+    public void limpiar(View v){
+        limpiar();
+    }
+
+    public int foto_aleatoria(){
+        int foto_seleccionada;
+        Random r= new Random();
+        foto_seleccionada = r.nextInt(this.fotos.size());
+        return fotos.get(foto_seleccionada);
+
+    }
+
+    public void limpiar(){
+        cedula.setText("");
+        nombre.setText("");
+        apellido.setText("");
+        cedula.requestFocus();
+    }
+
+    public void onBackPresed(){
+        finish();
+        Intent i = new Intent(AgregarPersona.this, MainActivity.class);
     }
 }
--- app/src/main/java/com/example/personas_material/MainActivity.java
+++ app/src/main/java/com/example/personas_material/MainActivity.java
@@ -1,16 +1,14 @@
 package com.example.personas_material;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,35 +19,16 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        FloatingActionButton fab = findViewById(R.id.btnAgregar);
+
+
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    public void agregar (View v){
+        Intent i;
+        i = new Intent(MainActivity.this, AgregarPersona.class);
+        startActivity(i);
+        finish();
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
